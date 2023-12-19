2dd586795281541e13fe960b0d12d3d4c0005286
vivektangudu2024 <tanguduvivek2003.com>
2023-12-19 20:27:01 +0530

    commit message

---
+++ lineComparision.java
@@ -0,0 +1,10 @@
+package com.day4;
+
+import java.util.Scanner;
+
+public class lineComparision {
+    public static void main(String[] args) {
+        // Display welcome message
+        System.out.println("Welcome to Line Comparison Computation Program!");
+    }
+}
